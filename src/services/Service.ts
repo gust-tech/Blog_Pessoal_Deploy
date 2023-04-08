c7ca3157fd5fd35c1029d317a3e84d817b93639d
Gustavo Rocha <111547125+gust-tech@users.noreply.github.com>
2023-04-08 19:25:35 -0300

    Update Service.ts

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://gustworld.onrender.com'
+    baseURL: 'https://dull-red-bullfrog.cyclic.app'
 })
 
     export const cadastroUsuario = async(url: any,dados: any,setDado: any) => { 
